e0f48676ad23ebca948f183aa60ccd0637891474
Yuri Sementsov <dev@smcv.me>
2019-08-19 19:02:42 +0300

    Init

---
+++ index.js
@@ -0,0 +1,29 @@
+
+module.exports = ({ addUtilities, variants, theme }) => {
+  const spacing = theme('spacing', {});
+  const ratio = 1.7;
+  let css = {};
+
+  Object.keys(spacing).forEach(name => {
+    let value = spacing[name];
+
+    css[`.inset-space-${name}`] = { padding: `${value}` };
+    css[`.inset-squish-space-${name}`] = {
+      padding: `calc(${value} / ${ratio} - 1px) ${value} calc(${value} / ${ratio})`,
+    };
+    css[`.stack-space-${name}`] = {
+      'margin-bottom': `${value}`,
+      '&:last-child': {
+        'margin-bottom': 0,
+      },
+    };
+    css[`.inline-space-${name}`] = {
+      'margin-right': `${value}`,
+      '&:last-child': {
+        'margin-right': 0,
+      },
+    };
+  });
+
+  addUtilities(css, variants('spacing', ['responsive']));
+};
